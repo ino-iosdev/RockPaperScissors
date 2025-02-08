c1c92ef8def9f8269783adf9a810d20c85a2345d
ino yang popper <ino.yang.p@gmail.com>
2025-02-08 12:44:12 -0800

    Created RockPaperScissors

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Ino Yang Popper on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
